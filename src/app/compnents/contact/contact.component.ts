e4c43243fa1ef740a1c63bd5a7a29d7082a44654
razi1999 <m.raziuddin99@gmail.com>
2023-08-13 23:23:38 +0500

    Validation in Contact

--- src/app/compnents/contact/contact.component.ts
+++ src/app/compnents/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,18 +9,30 @@ import { Router } from '@angular/router';
 })
 export class ContactComponent {
 
-  name: string = "";
-  phone: string = "";
-  email: string = "";
-  message: string = "";
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]+')]),
+    email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')]),
+    phone: new FormControl('', [Validators.required, Validators.pattern('[0-9]+')]),
+    message: new FormControl('', [Validators.required])
+  });
+  isSubmitted: boolean = false;
 
   constructor(private router: Router) { }
 
-  submit() {
-    if (this.name !== "" && this.phone !== "" && this.email !== "" && this.message !== "") {
-      console.log({ name: this.name, email: this.email, phone: this.phone, message: this.message });
-      this.router.navigateByUrl('/');
-    }
-  }
+  get name() { return this.form.get("name"); }
+  get email() { return this.form.get("email"); }
+  get phone() { return this.form.get("phone"); }
+  get message() { return this.form.get("message"); }
 
+  submit() {
+    console.log(this.form.value);
+    this.isSubmitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+    this.router.navigateByUrl('/');
+  }
 }
+
+
