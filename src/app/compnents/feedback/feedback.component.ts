913de9b207da5fc0ef09e4471f94a44b95f4e419
razi1999 <m.raziuddin99@gmail.com>
2023-08-13 22:52:29 +0500

    Validation in Feedback Form

--- src/app/compnents/feedback/feedback.component.ts
+++ src/app/compnents/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,17 +9,28 @@ import { Router } from '@angular/router';
 })
 export class FeedbackComponent {
 
-  fName: string = "";
-  lName: string = "";
-  email: string = "";
-  message: string = "";
+  form = new FormGroup({
+    fName: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+')]),
+    lName: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+')]),
+    email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')]),
+    message: new FormControl('', [Validators.required])
+  });
+  isSubmitted: boolean = false;
 
-  constructor(private router: Router) {  }
+  constructor(private router: Router) { }
+
+  get fName() { return this.form.get("fName"); }
+  get lName() { return this.form.get("lName"); }
+  get email() { return this.form.get("email"); }
+  get message() { return this.form.get("message"); }
 
   submit() {
-    if (this.fName !== "" && this.lName !== "" && this.email !== "" && this.message !== "") {
-      console.log({ firstName: this.fName, lastName: this.lName, email: this.email, message: this.message });
-      this.router.navigateByUrl('/');
+    console.log(this.form.value);
+    this.isSubmitted = true;
+
+    if (this.form.invalid) {
+      return;
     }
+    this.router.navigateByUrl('/');
   }
 }
